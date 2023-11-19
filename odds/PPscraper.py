a012ef5490e84b469588b3dbeb2a07282d7e60e5
Kevin Schroeder <95885765+KevSchroeder@users.noreply.github.com>
2023-11-19 08:46:48 -0500

    successfull fixed webdriver

--- odds/PPscraper.py
+++ odds/PPscraper.py
@@ -5,11 +5,11 @@ from selenium.webdriver. support import expected_conditions as EC
 import time
 import pandas as pd
 
-PATH = './chromedriver'
-driver = webdriver.Chrome(PATH)
+#PATH = open(r"C:\Users\chromedriver.exe")
+driver = webdriver.Chrome()
 
 driver.get("https://app.prizepicks.com/")
 
-wait = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "close")))
+wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "close")))
 driver.finde_element(By.XPATH, '/html/body/div[2]/div[3]/div/div.div[3]/button').click()
 time.sleep(10)
